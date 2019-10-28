ef42c9af95c0d5cbb2cfcae1d490ffbeaae380bc
dealson <dylan.vandenkoornhuyse@epitech.eu>
2019-10-28 15:52:23 +0100

    Task socket

--- src/Websocket/Task.ts
+++ src/Websocket/Task.ts
@@ -1,5 +1,10 @@
 import { Server, Socket } from 'socket.io';
 
+const userSocket: { [id in number]: Socket } = {};
+
+let taskSocket: { [research_id in number]: Socket } = {};
+
+
 
 function onNewConnection(socket: Socket) {
     console.log('new Connection');
@@ -8,13 +13,17 @@ function onNewConnection(socket: Socket) {
         socket.disconnect();
     }
 
+    socket.on('createTask', (task) => {
 
-    export function listen(io: Server): void {
+    });
+}
+
+export function listen(io: Server): void {
     // Language
-    io.use(initI18n);
+    // io.use(initI18n);
 
     // Auth
-    io.use(authSocket);
+    // io.use(authSocket);
 
     io.on('connection', onNewConnection);
 }
