41738424224fa1cb3bf86552aabfcad359719d37
dealson <dylan.vandenkoornhuyse@epitech.eu>
2019-11-10 00:10:55 +0100

    vasy test reever

--- src/REST/Routes/UserRoutes.ts
+++ src/REST/Routes/UserRoutes.ts
@@ -1,6 +1,6 @@
 import * as Router from 'koa-router';
 import { getAllUser, getUserByEmail } from '../Controlers/UserControler';
-import { retrieveUser } from '../../Utils/retrieveUser';
+import { retrieveUser, verifyToken } from '../../Utils/retrieveUser';
 import { getRepository } from 'typeorm';
 import { Task } from '../../Models/entity/Card';
 
@@ -10,11 +10,11 @@ export const userRoute = Router({
 
 userRoute.get('/all', getAllUser);
 
-userRoute.get('/me', async (ctx) => {
+userRoute.get('/me', verifyToken, async (ctx) => {
     console.log(ctx.request);
     ctx.body = await retrieveUser(ctx.request.header['authorization']);
 });
 
 userRoute.get('/task', async (ctx) => {
-   ctx.body = await getRepository(Task).find();
+   ctx.body = await getRepository(Task).find({relations: ["users", "teams"]});
 });
